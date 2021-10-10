2afc7a6bdead76e320b43449a855b1bde52bad19
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-10 19:27:34 +0200

    vpc config for test environment

--- environment/test/global/variables.tf
+++ environment/test/global/variables.tf
@@ -8,3 +8,23 @@ variable "terraform-bucket-name" {
   type    = string
 
 }
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
