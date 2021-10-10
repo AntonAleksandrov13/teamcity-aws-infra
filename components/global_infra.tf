7cd7a28080cacfa86948c1884595750244a6a0b0
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-10 19:27:07 +0200

    added vpc module as part of global infra

--- components/global_infra.tf
+++ components/global_infra.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.region
 }
+
+module "vpc" {
+  source = "../../../modules/global/vpc"
+  vpc_name = var.vpc_name
+  cidr = var.cidr
+  azs = var.azs
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
+}
