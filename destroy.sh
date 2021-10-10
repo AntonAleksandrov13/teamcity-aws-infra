02c8e62bea7ba4e11ae129f9c33e13289b55340f
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-10 21:02:41 +0200

    adding auto-approve to destroy script

--- destroy.sh
+++ destroy.sh
@@ -3,4 +3,4 @@ env=$1
 
 echo "Running terraform apply in environment: ${env}"
 echo "Working on global..."
-cd ./environment/$env/global && terraform destroy
+cd ./environment/$env/global && terraform destroy -auto-approve
