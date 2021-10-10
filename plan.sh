12172b13a5c7c002ccdf8da8773e2df97aaa7a89
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-10 20:05:26 +0200

    Experimenting with plan operation: added backend config to plan

--- plan.sh
+++ plan.sh
@@ -3,4 +3,4 @@ env=$1
 
 echo "Running terraform plan in environment: ${env}"
 echo "Working on global..."
-cd ./environment/$env/global && terraform plan -out=out.json -no-color
+cd ./environment/$env/global && terraform plan -out=out.json -no-color -backend-config=../../../backends/$env.tfvars
