02fda058fa1115aee4be313b40d356ebfb0bcf71
aalek3_nike <anton.aleksandrov@nike.com>
2021-10-11 18:15:56 +0200

    Added route53 module

--- modules/global/route53/main.tf
+++ modules/global/route53/main.tf
@@ -1,14 +1,7 @@
+
 module "zones" {
   source  = "terraform-aws-modules/route53/aws//modules/zones"
   version = "~> 2.0"
 
-  zones = {
-    "teamcity-anton-cloud.com" = {
-      comment = ""
-    }
-  }
-
-  tags = {
-    ManagedBy = "Terraform"
-  }
+  zones = var.zones
 }
---
+++ modules/global/route53/variables.tf
@@ -0,0 +1,4 @@
+variable "zones" {
+  type    = any
+  default = {}
+}
