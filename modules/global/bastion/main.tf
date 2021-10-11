9f8ee38d7630d6afdf8c1cacf5c745c53c0bd766
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-11 22:25:02 +0200

    added key pait generation + attempt to store values in Vault

--- modules/global/bastion/main.tf
+++ modules/global/bastion/main.tf
@@ -6,8 +6,32 @@ module "ec2-bastion-server" {
   subnets  = var.subnets
   key_name = aws_key_pair.bastion.id
 }
-
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+#tricky bit. on one hand, I would rather generate this by hand rather than storing it in state and showing it in plan
+# on the otherhand, we are focusing on reproducible environment here
 resource "aws_key_pair" "bastion" {
   key_name   = "bastion-key"
-  public_key = var.public_key
+  public_key = tls_private_key.pk.public_key_openssh
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
+  }
 }
+# TODO: fix storing secrets inside Vault
+# provider "vault" {
+
+# }
+
+
+# resource "vault_generic_secret" "bastion-ssh" {
+#   path = "secret/bastion"
+
+#   data_json = <<EOT
+# {
+# "private_key":   "${replace(tls_private_key.pk.private_key_pem, "\n", "\\n")}"
+# }
+# EOT
+# }
+
