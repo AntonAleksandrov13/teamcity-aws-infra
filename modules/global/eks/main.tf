6ec9c2f753afd7e40caf20c05c0fecc71bda3bf0
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-12 15:29:31 +0200

    added key-pair creation for eks workers

--- modules/global/eks/main.tf
+++ modules/global/eks/main.tf
@@ -17,7 +17,30 @@ module "eks" {
 
   node_groups = var.node_groups
 
-  worker_groups = var.worker_groups
+  worker_groups = [
+    for subnet in data.aws_subnet_ids.unroutable.ids :
+    {
+      key_name             = aws_key_pair.eks-worker-key.id
+      instance_type        = "t2.micro"
+      asg_max_size         = 1
+      asg_desired_capacity = 1
+      asg_min_size         = 1
+      subnets              = [subnet]
+      kubelet_extra_args   = "--node-labels=cluster=${var.cluster_name} --kube-reserved cpu=250m,memory=1Gi,ephemeral-storage=1G --system-reserved cpu=250m,memory=0.2Gi,ephemeral-storage=1Gi --eviction-hard memory.available<500Mi,nodefs.available<10%"
+      tags = [
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/enabled"
+          "propagate_at_launch" = "false"
+          "value"               = "true"
+        },
+        {
+          "key"                 = "k8s.io/cluster-autoscaler/${var.cluster_name}"
+          "propagate_at_launch" = "false"
+          "value"               = "true"
+        }
+      ]
+    }
+  ]
 
   worker_groups_launch_template = var.worker_groups_launch_template
 
@@ -26,4 +49,18 @@ module "eks" {
   }
 
   tags = var.tags
+}
+
+resource "tls_private_key" "pk" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+#tricky bit. on one hand, I would rather generate this by hand rather than storing it in state and showing it in plan
+# on the otherhand, we are focusing on reproducible environment here
+resource "aws_key_pair" "eks-worker-key" {
+  key_name   = "eks-worker-key"
+  public_key = tls_private_key.pk.public_key_openssh
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./eks-worker-key.pem"
+  }
 }
