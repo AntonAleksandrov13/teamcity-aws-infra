5490b03b825840eb545c6a772e55e821f5d9b676
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-13 16:37:03 +0200

    fixed kubernetes dependency cycle

--- modules/global/eks/main.tf
+++ modules/global/eks/main.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "eks" {
-  name = var.cluster_name
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "eks" {
-  name = var.cluster_name
+  name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
