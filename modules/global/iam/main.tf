8303116594231ba9a3a931d4370b61760ec92d5b
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-13 18:49:00 +0200

    external dns role and policy created

--- modules/global/iam/main.tf
+++ modules/global/iam/main.tf
@@ -51,3 +51,59 @@ module "cluster-autoscaler-policy" {
 }
 EOF
 }
+
+module "external-dns-role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "~> 4.3"
+
+  create_role = true
+
+  role_name = "external-dns"
+
+  tags = {
+    Role = "external-dns-with-oidc"
+  }
+
+  provider_url = local.provider_url
+
+  role_policy_arns = [
+    module.external-dns-policy.arn,
+  ]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:external-dns"]
+}
+
+module "external-dns-policy" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "~> 4.3"
+
+  name        = "external-dns-policy"
+  path        = "/"
+  description = "Allow external dns to CRUD A and TXT records"
+
+  policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ChangeResourceRecordSets"
+     ],
+     "Resource": [
+       "arn:aws:route53:::hostedzone/*"
+     ]
+   },
+   {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ListHostedZones",
+       "route53:ListResourceRecordSets"
+     ],
+     "Resource": [
+       "*"
+     ]
+   }
+ ]
+}
+EOF
+}
