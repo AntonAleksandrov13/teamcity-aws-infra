8013921945410b81c1ca00ac0b6283313721ca55
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-13 18:53:56 +0200

    external-dns_role_arn -> external_dns_role_arn rename

--- modules/global/iam/outputs.tf
+++ modules/global/iam/outputs.tf
@@ -2,6 +2,6 @@ output "cluster_autoscaler_role_arn" {
   value = module.cluster-autoscaler-role.iam_role_arn
 }
 
-output "external-dns_role_arn" {
+output "external_dns_role_arn" {
   value = module.external-dns-role.iam_role_arn
 }
