12e498bbb2e203135d70c33ed2a1af2fba47ce6b
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-13 18:52:11 +0200

    fix missing output in iam module

--- modules/global/iam/outputs.tf
+++ modules/global/iam/outputs.tf
@@ -1,3 +1,7 @@
 output "cluster_autoscaler_role_arn" {
-    value = module.cluster-autoscaler-role.iam_role_arn
+  value = module.cluster-autoscaler-role.iam_role_arn
+}
+
+output "external-dns_role_arn" {
+  value = module.external-dns-role.iam_role_arn
 }
