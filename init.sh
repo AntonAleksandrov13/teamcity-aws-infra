c0d87a805666ce6baf4d4b54701e6a14d8dc568a
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-14 15:03:07 +0200

    Updated supporting script to match tenant infra components

--- init.sh
+++ init.sh
@@ -4,3 +4,10 @@ env=$1
 echo "Running terraform init in environment: ${env}"
 echo "Working on global..."
 cd ./environment/$env/global && yes | rm -r ./.terraform* || true && terraform init -backend-config=../../../backends/$env.tfvars
+echo "Working on tenants..."
+for f in ../../../environment/$env/*; do
+    if [ "$f" != "../../../environment/$env/global" ]; then
+        echo "Working on $f"
+        cd $f && terraform init -backend-config=../../../backends/$env.tfvars
+    fi
+done
--- validate.sh
+++ validate.sh
@@ -4,3 +4,10 @@ env=$1
 echo "Running terraform validate in environment: ${env}"
 echo "Working on global..."
 cd ./environment/$env/global && terraform validate -no-color
+echo "Working on tenants..."
+for f in ../../../environment/$env/*; do
+    if [ "$f" != "../../../environment/$env/global" ]; then
+        echo "Working on $f"
+        cd $f && terraform validate -no-color
+    fi
+done
