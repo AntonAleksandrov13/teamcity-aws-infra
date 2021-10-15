cb182e4ce6c25fb6f5ae395fe7eb7447677b90ca
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-15 21:09:52 +0200

    added a simple tenant bucket module(loggin is disabled)

---
+++ modules/shared/s3/main.tf
@@ -0,0 +1,19 @@
+locals {
+  bucket_name = "${replace(var.tenant_name, "/\\W|_|\\s/", "")}-artifact-bucket"
+}
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = local.bucket_name
+  acl    = "private"
+
+#todo enable in the future
+# logging = {
+#     target_bucket = module.log_bucket.s3_bucket_id
+#     target_prefix = "log/"
+# }
+  versioning = {
+    enabled = false
+  }
+
+}
