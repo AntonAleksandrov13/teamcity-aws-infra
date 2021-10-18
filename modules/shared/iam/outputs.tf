f985ec6f7ed789bb0016bb556a267f58cc305df6
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-18 20:11:51 +0200

    fixed iam module output to give a valid arn

--- modules/shared/iam/outputs.tf
+++ modules/shared/iam/outputs.tf
@@ -1,6 +1,6 @@
 output "server_role_arn" {
-  value = module.tenant-server-role.iam_role_name
+  value = module.tenant-server-role.iam_role_arn
 }
 output "agent_role_arn" {
-  value = module.tenant-agent-role.iam_role_name
+  value = module.tenant-agent-role.iam_role_arn
 }
