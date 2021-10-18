453c2384b0c56c95fb91c350ac361deaa56c0f48
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-19 00:08:24 +0200

    aws_efs_csi_driver_role_arn output added to global iam

--- modules/global/iam/outputs.tf
+++ modules/global/iam/outputs.tf
@@ -6,6 +6,6 @@ output "external_dns_role_arn" {
   value = module.external-dns-role.iam_role_arn
 }
 
-output "aws-efs-csi-driver-role" {
-  value = module.aws-efs-csi-role.iam_role_arn
+output "aws_efs_csi_driver_role_arn" {
+  value = module.aws-efs-csi-driver-role.iam_role_arn
 }
