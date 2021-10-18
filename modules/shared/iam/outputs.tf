9f6efbf2bd53545435917b18de54eb4c9c2fb3fc
AntonAleksandrov13 <antonaleksandrov24@gmail.com>
2021-10-18 16:29:06 +0200

    fixed reference to tenant-server-role

--- modules/shared/iam/outputs.tf
+++ modules/shared/iam/outputs.tf
@@ -1,5 +1,5 @@
 output "server_role_arn" {
-  value = module.server-agent-role.iam_role_name
+  value = module.tenant-server-role.iam_role_name
 }
 output "agent_role_arn" {
   value = module.tenant-agent-role.iam_role_name
